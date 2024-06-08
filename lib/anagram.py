026ab42279a7076fd73ab3cd9bc4a4d0e2e3b339
Tess <tess.mueske@gmail.com>
2024-06-08 12:08:38 +0200

    complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
-# your code goes here!
+class Anagram:
+
+    def __init__(self, word="word"):
+        self.word = word
+
+    def match(self, anagram_list):
+        anagrams = []
+        for anagram in anagram_list:
+            if sorted(self.word) == sorted(anagram):
+                anagrams.append(anagram)
+        return anagrams
